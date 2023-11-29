56d9258496db2ee0689bd394221f92318a684491
WillianCosta <willian95@outlook.com>
2023-11-29 09:21:11 -0300

    alteração base url

--- helpers/base-url-api.js
+++ helpers/base-url-api.js
@@ -1,5 +1,5 @@
 const baseUrl =
-  "https://cashquest-jrr20nwxs-costaowillians-projects.vercel.app/api";
+  "https://cashquest-e553gh6km-costaowillians-projects.vercel.app/api";
 
 //14/11/2023
 // "https://cashquest-k7bodh9hj-costaowillians-projects.vercel.app/api"
